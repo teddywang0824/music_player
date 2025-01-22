a4c8cadb1e7db95e60667c747991e36c07803c1b
teddy <htc0824htc@gmail.com>
2025-01-22 12:26:28 +0800

    導入下載邏輯 以及 儲存空間許可

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'view/musiclist.dart';
+// import 'view/musiclist.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,7 +39,7 @@ class Home extends StatelessWidget {
           style: TextStyle(fontSize: 18),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
         ],
       ),
       body: AudioDownload(),
@@ -53,10 +56,11 @@ class AudioDownload extends StatefulWidget {
 
 class _AudioDownloadState extends State<AudioDownload> {
   final TextEditingController _urlController = TextEditingController();
-  String _status = '';
+  String _status = ''; //指示
+  bool _isLoading = false;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -66,21 +70,68 @@ class _AudioDownloadState extends State<AudioDownload> {
             labelText: '輸入 Youtube 網址',
           ),
         ),
-        const SizedBox(height: 20,),
-        ElevatedButton(onPressed: (){
-          _testPathProvider();
-          debugPrint("-----你輸入的是 ${_urlController.text}-----");
-          setState(() {
-            _status = '喔是喔';
-          });
-        }, child: const Text("送出")),
+        const SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+            onPressed: _isLoading
+                ? null
+                : () {
+                    _download(_urlController.text);
+                  },
+            child: Text(_isLoading ? "處理中" : "下載")),
+        const SizedBox(
+          height: 20,
+        ),
         Text(_status),
       ],
     );
   }
 
-  Future<void> _testPathProvider() async {
-    var dir = await getApplicationDocumentsDirectory();
-    debugPrint("---${dir.path}---");
+  // Future<void> _testPathProvider() async {
+  //   var dir = await getApplicationDocumentsDirectory();
+  //   debugPrint("---${dir.path}---");
+  // }
+
+  Future<void> _download(String url) async {
+    setState(() {
+      _isLoading = true;
+      _status = '開始下載，請勿關閉...';
+    });
+
+    try {
+      var state = await Permission.storage.request();
+      if(!state.isGranted) {
+        throw '需要儲存權限';
+      }
+
+      final yt = YoutubeExplode();
+
+      final video = await yt.videos.get(url);
+      final videoTitle = video.title;
+
+      var manifest = await yt.videos.streams.getManifest(url);
+      var audio = manifest.audioOnly.withHighestBitrate();
+
+      var dir = await getExternalStorageDirectory();
+      setState(() => _status = '下載中，請勿關閉...');
+      debugPrint(dir!.path);
+
+      var stream = yt.videos.streams.get(audio);
+      final file = File('${dir.path}/$videoTitle.mp3');
+
+      var fileStream = file.openWrite();
+
+      await stream.pipe(fileStream);
+      await fileStream.flush;
+      await fileStream.close();
+      yt.close();
+
+      setState(() => _status = "已經下載完成");
+    } catch (e) {
+      setState(() => _status = '錯誤: $e');
+    } finally {
+      setState(() => _isLoading = false);
+    }
   }
 }
