fdb5be0db1f5fdc735a4a2d6958406b067bcb647
teddy <htc0824htc@gmail.com>
2025-01-23 15:00:42 +0800

    ListView顯示已下載的音樂

--- lib/view/musiclist.dart
+++ lib/view/musiclist.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-class MusicList extends StatelessWidget {
+class MusicList extends StatefulWidget {
   const MusicList({super.key});
 
   @override
+  State<MusicList> createState() => _MusicListState();
+}
+
+class _MusicListState extends State<MusicList> {
+  List<String> fileNames = [];
+  String states = '讀取中...';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFileNames();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("我的音樂"),
-            leading: const Icon(Icons.music_note),
-            trailing: const Icon(Icons.play_arrow),
-            onTap: (){},
-            onLongPress: (){},
-          );
-        });
+    return fileNames.isEmpty
+        ? Center(child: Text(states))
+        : ListView.builder(
+            itemCount: fileNames.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(fileNames[index]),
+                leading: const Icon(Icons.music_note),
+                trailing: const Icon(Icons.play_arrow),
+                onTap: () {},
+                onLongPress: () {},
+              );
+            });
+  }
+
+  Future<void> _loadFileNames() async {
+    try {
+      var dir = await getExternalStorageDirectory();
+
+      List<FileSystemEntity> files = dir!.listSync();
+      List<String> names = files
+          .whereType<File>()
+          .map((file) => file.path.split('/').last)
+          .toList();
+
+      setState(() {
+        fileNames = names;
+        states = names.isEmpty ? "資料夾中沒有檔案" : "檔案讀取完成";
+      });
+    } catch (e) {
+      setState(() => states = "發生錯誤 : $e");
+    }
   }
 }
